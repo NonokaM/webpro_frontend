6b4f4c3c33382879c4bc277ec8ac717d1e31024b
NonokaM <c1201071@gmail.com>
2023-11-27 03:27:52 +0900

    add: ページ遷移を追加

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { AuthFlagContext } from "@/providers/AuthFlagProvider";
+import Link from "next/link"
 
 export default function Header() {
   const { isAuth, setIsAuth } = useContext(AuthFlagContext);
@@ -8,6 +9,20 @@ export default function Header() {
   return (
     <>
       <p>header</p>
+
+      <nav>
+        <ul>
+        <Link href="/register">　/register</Link>
+        <Link href="/login">　/login</Link>
+
+        <Link href="/post/create">　/post/create</Link>
+        <Link href="/post/1">　/post/[id]</Link>
+
+        <Link href="/question/1">　/question/[id]</Link>
+
+        <Link href="/user/1">　/user/[id]</Link>
+        </ul>
+      </nav>
     </>
   )
 }
