dbeff527ac3cc35ad48ec73d9fe94b52f5ab3ee8
nagisa <c1318728@st.kanazawa-it.ac.jp>
2023-12-02 16:34:56 +0900

    タイムスタンプを登録できるようにしました

--- src/pages/post/create.jsx
+++ src/pages/post/create.jsx
@@ -1,49 +1,69 @@
-import { addDoc, collection } from "firebase/firestore";
-import { useState } from "react";
-import db, { firestore } from "@/lib/firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { firestore } from "@/lib/firebase";
+import Router from "next/router";
 
 export default function Create() {
-    const [image, setImage] = useState([]);
+    const [image, setImage] = useState(null);
     const [subject, setSubject] = useState("");
     const [department, setDepartment] = useState("");
     const [grade, setGrade] = useState("");
     const [year, setYear] = useState();
     const [overviw, setOverviw] = useState("");
-    const [errorMessage, setErrorMessage] = useState("")
-    const handleImage = event => {
-        const image = event.target.files[0];
-        setImage(image);
-    };
+    // const [timestamp, setTimestamp] = useState(new Date());
+    const [errorMessage, setErrorMessage] = useState("");
+    
+    //画像の処理
+    // const handleImage = event => {
+    //     const image = event.target.files[0];
+    //     setImage(image);
+    // };
 
     const doCreate = async () => {
-        if ( !image || !subject ) {
+        if ( !subject ) {
             setErrorMessage("必須の項目を入力してください");
             return;
         };
 
+        //画像の処理
+        // const changeImage = () => {
+        //     if (!e.target.files) return;
         
-        await addDoc(collection(firestore, db, "posts"),{
+        //     const imgObject = e.target.files[0];
+        //     setImage(window.URL.createObjectURL(imgObject))   
+        // };
+
+        await addDoc(collection(firestore, "posts"),{
             images: image,
             subject: subject,
             department: department,
             grade: grade,
             year: year,
             overviwd: overviw,
-            postTime: firebase.firestore.FieldValue.serverTimestamp(),
-        });           
-        router.push('/');
-    }
+            postTime: new Date(),
+        });
+        Router.push('/');
+    };
+
+    // useEffect(() => {
+    //     const getCurrentTimestamp = () => {
+    //         const timestamp = new Date();
+    //         setTimestamp(timestamp);
+    //     };
+
+    //     getCurrentTimestamp();
+    // }, []);
 
     return (
         <div>
             <h1>過去問を投稿</h1>
             <form doCreate={doCreate}>
-                <input
+                {/* <input
                     type="file"
                     name="image"
                     placeholder="過去問をアップロード"
-                    onChange={(e) => setImage(e.target.value)}
-                />
+                    onChange={handleImage}
+                /> */}
 
                 <input
                     type="text"
@@ -93,5 +113,5 @@ export default function Create() {
             {errorMessage && <p>{errorMessage}</p>}
             {/* <img src={imageUrl} alr="uploaded" /> */}
         </div>
-    )
+    );
 }
