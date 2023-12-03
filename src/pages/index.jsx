aa3a82c030a9d20f823dcabd548195fa39acf3a1
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2023-12-03 13:54:42 +0900

    homecss完了

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "@/lib/firebase";
 import Link from 'next/link';
+import styles from '@/styles/index.module.css';
 
 const TARGET_COLLECTION_NAME = "posts";
 
@@ -29,23 +30,32 @@ export default function Home() {
     <div>
       {data.length > 0 ? (
         data.map((item, i) => (
-          <Link
-          key={item.id}
-          href={{
-            pathname: `/post/${item.id}`,
-            query: { subject: item.subject, department: item.department, grade: item.grade,
-              year: item.year, overviw: item.overviw, postTime: item.postTime, images: item.images}
-          }}
+          <Link className={styles.link}
+            key={item.id}
+            href={{
+              pathname: `/post/${item.id}`,
+              query: {
+                subject: item.subject, department: item.department, grade: item.grade,
+                year: item.year, overviw: item.overviw, postTime: item.postTime, images: item.images
+              }
+            }}
           >
-          <p>{item.userName}</p>
-            <p>{item.subject}</p>
-            <p>{item.department}</p>
-            <p>{item.grade}</p>
-            <p>{item.year}</p>
-            <p>{item.overviw}</p>
-            {item.postTime && <p>{item.postTime.toDate().toString()}</p>}
-            {item.images && <img src={item.images} alt="Uploaded" />}
-            <button></button>
+            <div className={styles.big}>
+              <p>{item.userName}</p>
+              {item.postTime && <p className={styles.time}>{item.postTime.toDate().toString()}</p>}
+              <div className={styles.title}>
+                <p className={styles.kamoku}>{item.subject}</p>
+                <div className={styles.when}>
+                  <p className={styles.gakka}>{item.department}</p>
+                  <div className={styles.line}>/</div>
+                  <p className={styles.gakunen}>{item.grade}</p>
+                  <div className={styles.line}>/</div>
+                  <p className={styles.nendo}>{item.year}</p>
+                </div>
+              </div>
+              <p className={styles.gaiyou}>{item.overviw}</p>
+              {item.images && <img src={item.images} className={styles.img} alt="Uploaded" />}
+            </div>
           </Link>
         ))
       ) : (
