cfd0fd4a0a0f3a2e8495c68c1e367c84144c70fb
nonokaM <c1201071@gmail.com>
2023-12-03 10:21:08 +0900

    feat: 実際の流れでバックエンドからデータとれた！！かち！

--- src/pages/question/[id].jsx
+++ src/pages/question/[id].jsx
@@ -1,33 +1,51 @@
 import { useRouter } from 'next/router';
-import useGenerateURL from '@/hooks/useGenerateURL';
+import { useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import useGenerateQuestion from '@/hooks/useGenerateQuestion';
+import { firestore } from '@/lib/firebase';
 
 export default function PostPage() {
     const router = useRouter();
     const { id } = router.query;
+    const [imageUrl, setImageUrl] = useState('');
+    const { generateFunc, data } = useGenerateQuestion();
 
-    const filePath = `images/${id}.JPG`;
-    const { url, questionsData } = useGenerateURL(filePath);
+    useEffect(() => {
+        if (id) {
+            const docRef = doc(firestore, 'posts', id);
+            getDoc(docRef).then(docSnap => {
+                if (docSnap.exists()) {
+                    const imageData = docSnap.data();
+                    setImageUrl(imageData.images);
+                } else {
+                    console.log("No such document!");
+                }
+            }).catch(error => {
+                console.error("Error getting document:", error);
+            });
+        }
+    }, [id]);
 
-    if (!url || !questionsData) {
+    useEffect(() => {
+        if (imageUrl) {
+            generateFunc(imageUrl);
+        }
+    }, [imageUrl, generateFunc]);
+
+    if (!data) {
         return <div>Loading...</div>;
     }
 
     return (
         <div>
-            <p>About</p>
-            {url && <div>URL: {url}</div>}
-            {questionsData && (
-                <div>
-                    <h3>Questions</h3>
-                    {questionsData.questions.map((question, index) => (
-                        <p key={index}>{question}</p>
-                    ))}
-                    <h3>Answers</h3>
-                    {questionsData.answers.map((answer, index) => (
-                        <p key={index}>{answer}</p>
-                    ))}
-                </div>
-            )}
+            <h3>Questions</h3>
+            {data.questions && data.questions.map((question, index) => (
+                <p key={index}>{question}</p>
+            ))}
+            <h3>Answers</h3>
+            {data.answers && data.answers.map((answer, index) => (
+                <p key={index}>{answer}</p>
+            ))}
         </div>
     );
 }
